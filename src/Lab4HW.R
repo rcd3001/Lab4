4b1ef2b20383f80c5b5a06bd6ec538c30db249b3
rcd3001 <rcd3001@med.cornell.edu>
2018-10-09 21:27:50 -0400

    Created RMD for final submission

--- src/Lab4HW.R
+++ src/Lab4HW.R
@@ -21,21 +21,21 @@ ggplot(data = data.frame(x  = c(-166:167)), aes(x))+
   scale_x_continuous(breaks = c(qnorm(0.05), qnorm(0.95)), 
                      labels = c(expression(theta["0"]), expression(theta["a"])), 
                      limits = c(-5,5))+
-  stat_function(fun = dnorm, color = "blue", size = 1, 
+  stat_function(fun = dnorm, color = "blue", size = 1, xlim = c(-5,qnorm(0.95)), 
                 args = list(mean = qnorm(0.05), sd = 1))+
-  stat_function(fun = dnorm, color = "red", size = 1, 
+  stat_function(fun = dnorm, color = "red", size = 1, xlim = c(qnorm(0.05), 5), 
                 args = list(mean = qnorm(0.95), sd = 1))+
   geom_vline(xintercept = mean(c(qnorm(0.05), qnorm(0.95))), linetype = "dashed")+
-  geom_polygon(data = coord1DF, aes(x=coordX, y = coordY, fill = "Type II Error"), alpha = 0.5)+
-  geom_polygon(data = coord2DF, aes(x=coordX2, y = coordY2, fill = "Type I Error"), alpha = 0.5)+
+  geom_polygon(data = coord1DF, aes(x=coordX, y = coordY, fill = "     Type II Error"), alpha = 0.5)+
+  geom_polygon(data = coord2DF, aes(x=coordX2, y = coordY2, fill = "     Type I Error"), alpha = 0.5)+
   labs(x = NULL, y = NULL)+
   annotate("text", parse = TRUE, x = -0.4, y = .02, size = 6, 
            label = as.character(TeX('$\\beta')))+
   annotate("text", parse = TRUE, x = 0.4, y = .02, size = 6, color = "white", 
            label = as.character(TeX('$\\alpha')))+
   theme_classic()+
-  scale_fill_manual(labels = c("Type I Error", "Type II Error"), values = c("blue", "red"))+
-  theme(text = element_text(size = 15), legend.text = element_text(size = 15), 
+  scale_fill_manual(labels = c("     Type I Error", "     Type II Error"), values = c("blue", "red"), guide = guide_legend(override.aes=aes(color="black")))+
+    theme(text = element_text(size = 15), legend.text = element_text(size = 15), 
         legend.title = element_blank(), legend.box.background = element_rect(),
         legend.background = element_blank(), panel.border = element_rect(fill=NA, color="black", size = 0.4), 
         legend.position = c(0,1), legend.justification = c(0,1), axis.text.y = element_text(angle = 90, hjust = 0.5), 
